db7b461916171d8830515f4865ad7051019da196
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-11-20 12:17:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6282221792667','682221792667',''682221792667'] // Letakan nomor kamu disini
+global.owner = ['6282221792667','682221792667','682221792667'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
